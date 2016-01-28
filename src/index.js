172962d3cd7431992ce7903b21f86cecbdc657fb
Gio d'Amelio <giodamelio@gmail.com>
2016-01-27 21:53:02 -0800

    Basic react app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log(`1 + 100 = ${1 + 100}`);
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+render((
+  <App />
+), document.getElementById('app'));
