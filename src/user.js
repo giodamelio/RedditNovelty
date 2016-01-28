3497c4859f1258dc630456e024c96a64275e76e0
Gio d'Amelio <giodamelio@gmail.com>
2016-01-28 13:00:12 -0800

    Show comment body on hover

--- src/user.js
+++ src/user.js
@@ -15,25 +15,31 @@ export default class User extends React.Component {
       .then(response => response.json())
       .then(json => {
         _(json.data.children)
-          .map(comment => comment.data.body)
+          .map(comment => comment.data)
 
           // Get the imgur link out of the comment
           .map(comment => {
-            const image = comment.match(/(http:\/\/(.*)imgur.com\/(.*))(\?.*)?/);
+            const image = comment.body.match(/(http:\/\/(.*)imgur.com\/(.*))(\?.*)?/);
             if (image) {
-              return image[0];
+              return {
+                ...comment,
+                image: image[0],
+              };
             }
-            return null;
+            return comment;
           })
 
           // Filter out posts without an image link
-          .filter(comment => comment)
+          .filter(comment => comment.image)
 
           // Get the direct image link
           // Simply add an extension if there isnt one, imgur does the rest
           .map(comment => {
-            if (comment.substr(-4, 1) !== '.') {
-              return `${comment}.png`;
+            if (comment.image.substr(-4, 1) !== '.') {
+              return {
+                ...comment,
+                image: `${comment.image}.png`,
+              };
             }
             return comment;
           })
@@ -54,7 +60,11 @@ export default class User extends React.Component {
     return (
       <div className="center">
         {this.state.comments.map((comment, index) =>
-          <img key={index} src={comment} className="images" />
+          <img
+            key={index}
+            src={comment.image}
+            title={comment.body}
+            className="images" />
         )}
       </div>
     );
